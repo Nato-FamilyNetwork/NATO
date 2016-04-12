929d76e0dc29274b80c8688c382c656f3d6d5cf6
guesmiii <med.guesmi1992@gmail.com>
2016-04-12 18:08:42 +0200

    calendar
    done

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -633,6 +633,260 @@ today = 'date : '+mm+'/'+dd+'/'+yyyy+' time : '+h+' h '+m+' m '+s+' s ';
     addd();
    
         
+})
+
+.controller('calendarController',function($scope, $resource, $http,$route){
+    jQuery(function($) {
+
+/* initialize the external events
+	-----------------------------------------------------------------*/
+
+	$('#external-events div.external-event').each(function() {
+
+		// create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
+		// it doesn't need to have a start or end
+		var eventObject = {
+			title: $.trim($(this).text()) // use the element's text as the event title
+		};
+
+		// store the Event Object in the DOM element so we can get to it later
+		$(this).data('eventObject', eventObject);
+
+		// make the event draggable using jQuery UI
+		$(this).draggable({
+			zIndex: 999,
+			revert: true,      // will cause the event to go back to its
+			revertDuration: 0  //  original position after the drag
+		});
+		
+	});
+
+
+
+
+	/* initialize the calendar
+	-----------------------------------------------------------------*/
+
+	var date = new Date();
+	var d = date.getDate();
+	var m = date.getMonth();
+	var y = date.getFullYear();
+        var aa=new Array;   
+        var cal=$resource('http://127.0.0.1:3000/calendar/');
+        //getAll
+        $scope.calendar=cal.query();
+        $scope.calendar.$promise.then(function(value) {
+            for(var i=0 ;i<value.length; i++){
+                if (value[i].end=="null"){
+                    value[i].end=null
+                }
+                aa.push({title:value[i].title, start: value[i].start,
+			end: value[i].end,className:value[i].className});
+               
+            
+                if(i+1 == value.length){
+	var calendar = $('#calendar').fullCalendar({
+		//isRTL: true,
+		//firstDay: 1,// >> change first day of week 
+		 buttonHtml: {
+			prev: '<i class="ace-icon fa fa-chevron-left"></i>',
+			next: '<i class="ace-icon fa fa-chevron-right"></i>'
+		},
+	
+		header: {
+			left: 'prev,next today',
+			center: 'title',
+			right: 'month,agendaWeek,agendaDay'
+		},      
+		events: 
+            aa
+		
+		,
+		
+		/**eventResize: function(event, delta, revertFunc) {
+
+			alert(event.title + " end is now " + event.end.format());
+
+			if (!confirm("is this okay?")) {
+				revertFunc();
+			}
+
+		},*/
+		
+		editable: true,
+		droppable: true, // this allows things to be dropped onto the calendar !!!
+		drop: function(date) { // this function is called when something is dropped
+		
+			// retrieve the dropped element's stored Event Object
+			var originalEventObject = $(this).data('eventObject');
+			var $extraEventClass = $(this).attr('data-class');
+			
+			
+			// we need to copy it, so that multiple events don't have a reference to the same object
+			var copiedEventObject = $.extend({}, originalEventObject);
+			
+			// assign it the date that was reported
+			copiedEventObject.start = date;
+			copiedEventObject.allDay = false;
+			if($extraEventClass) copiedEventObject['className'] = [$extraEventClass];
+			
+			// render the event on the calendar
+			// the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
+			$('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
+            var title = "new event";
+            var start = date;
+            var end = null;
+            var className = [$extraEventClass];
+			$scope.formEvent={title,start,end,className };
+                    $http.post('http://127.0.0.1:3000/calendar/addEvent',$scope.formEvent).
+        success(function(data) {
+            console.log("posted successfully");
+        }).error(function(data) {
+            console.error("error in posting");
+        })
+    $route.reload();
+			// is the "remove after drop" checkbox checked?
+			if ($('#drop-remove').is(':checked')) {
+				// if so, remove the element from the "Draggable Events" list
+				$(this).remove();
+			}
+            
+			
+		}
+		,
+		selectable: true,
+		selectHelper: true,
+		select: function(start, end, allDay) {
+			var className ='label-info' ;
+			bootbox.prompt("New Event Title:", function(title) {
+				if (title !== null) {
+					calendar.fullCalendar('renderEvent',
+						{
+							title: title,
+							start: start,
+							end: end,
+							allDay: allDay,
+							className: 'label-info'
+						},
+						true // make the event "stick"
+                                          
+					);
+                    
+                    $scope.formEvent={title,start,end,className };
+                    $http.post('http://127.0.0.1:3000/calendar/addEvent',$scope.formEvent).
+        success(function(data) {
+            console.log("posted successfully");
+        }).error(function(data) {
+            console.error("error in posting");
+        })
+   $route.reload();
+				}
+			});
+			
+
+			calendar.fullCalendar('unselect');
+		}
+		,
+		eventClick: function(calEvent, jsEvent, view) {
+
+			//display a modal
+			var modal = 
+			'<div class="modal fade">\
+			  <div class="modal-dialog">\
+			   <div class="modal-content">\
+				 <div class="modal-body">\
+				   <button type="button" class="close" data-dismiss="modal" style="margin-top:-10px;">&times;</button>\
+				   <form class="no-margin">\
+					  <label>Change event name &nbsp;</label>\
+					  <input class="middle" autocomplete="off" type="text" value="' + calEvent.title + '" />\
+					 <button type="submit" class="btn btn-sm btn-success"><i class="ace-icon fa fa-check"></i> Save</button>\
+				   </form>\
+				 </div>\
+				 <div class="modal-footer">\
+					<button type="button" class="btn btn-sm btn-danger" data-action="delete"><i class="ace-icon fa fa-trash-o"></i> Delete Event</button>\
+					<button type="button" class="btn btn-sm" data-dismiss="modal"><i class="ace-icon fa fa-times"></i> Cancel</button>\
+				 </div>\
+			  </div>\
+			 </div>\
+			</div>';
+		
+		
+			var modal = $(modal).appendTo('body');
+			modal.find('form').on('submit', function(ev){
+				ev.preventDefault();
+var abbb = $(this).find("input[type=text]").val();
+console.log(abbb);
+                
+                 for(var k=0;k<value.length;k++){
+                if((calEvent.className == value[k].className) &&(calEvent.title==value[k].title)){
+                /* var title=abbb;
+                 var start= value[k].start;
+                 var end= value[k].end;
+                 var className=value[k].className;
+                    $scope.formDat ={title,start,end,className};*/
+                    var c = value[k].end;
+                    if (c == null){
+                    c="null";
+                    }
+                    console.log(abbb);
+         var up=$resource('http://127.0.0.1:3000/calendar/:id/:title/:start/:end/:className', {}, {
+      query: {method:'PUT', params:{id:value[k]._id,title:abbb,start:value[k].start,end:c,className:value[k].className}, isArray:false}});
+                    console.log(abbb);
+	  up.query();
+           console.log('ok');
+                }
+                };
+                
+                
+                
+                
+                
+                
+                calEvent.title= abbb;
+                
+				
+                
+				calendar.fullCalendar('updateEvent', calEvent);
+                
+				modal.modal("hide");
+			});
+			modal.find('button[data-action=delete]').on('click', function() {
+				calendar.fullCalendar('removeEvents' , function(ev){
+					return (ev._id == calEvent._id);
+				})
+                for(var j=0;j<value.length;j++){
+                if((calEvent.className == value[j].className) &&(calEvent.title==value[j].title)){
+                
+                   
+         var del=$resource('http://127.0.0.1:3000/calendar/:id', {}, {
+      query: {method:'DELETE', params:{id:value[j]._id}, isArray:false}});
+	  del.query();
+           
+                }
+                };
+                
+				modal.modal("hide");
+			});
+			
+			modal.modal('show').on('hidden', function(){
+				modal.remove();
+			});
+
+
+			
+
+			// change the border color just for fun
+			//$(this).css('border-color', 'red');
+
+		}
+		
+	});}
+            };});
+
+
+})
+	
+      
 });
 
 
