e967aa121804485370dd92e08214b5c15d705a0d
Zoolixx <mahdouch_clubiste@hotmail.fr>
2016-05-03 13:39:44 +0100

    cmt

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -4,10 +4,9 @@ angular.module('familyNetworkApp.services', []).factory('twitterService', functi
 
     return {
         initialize: function() {
-            //initialize OAuth.io with public key of the application
+            //initialize OAuth.io avec public key
 		
-            OAuth.initialize('kgWIP9pNqk7xy14Izcm3mOff0IQ', {cache:true});
-            //try to create an authorization result when the page loads, this means a returning user won't have to click the twitter button again
+            OAuth.initialize('kgWIP9pNqk7xy14Izcm3mOff0IQ', {cache:true})
             authorizationResult = OAuth.create('twitter');
             authResult= OAuth.create('instagram');
         },
@@ -16,12 +15,13 @@ angular.module('familyNetworkApp.services', []).factory('twitterService', functi
         },
         connectTwitter: function() {
             var deferred = $q.defer();
-            OAuth.popup('twitter', {cache:true}, function(error, result) { //cache means to execute the callback if the tokens are already present
+            OAuth.popup('twitter', {cache:true}, function(error, result) { 
+                //cache pour sauvegarder le token
                 if (!error) {
                     authorizationResult = result;
                     deferred.resolve();
                 } else {
-                    //do something if there's an error
+                    console.log(error);
                 }
             });
             return deferred.promise;
@@ -31,10 +31,10 @@ angular.module('familyNetworkApp.services', []).factory('twitterService', functi
             authorizationResult = false;
         },
         getLatestTweets: function () {
-            //create a deferred object using Angular's $q service
+            //create a deferred object using $q
             var deferred = $q.defer();
             var promise = authorizationResult.get('/1.1/statuses/home_timeline.json').done(function(data) { //https://dev.twitter.com/docs/api/1.1/get/statuses/home_timeline
-                //when the data is retrieved resolved the deferred object
+                //resolve l'objet quand done
                 deferred.resolve(data)
             });
             //return the promise of the deferred object
@@ -47,7 +47,7 @@ angular.module('familyNetworkApp.services', []).factory('twitterService', functi
         
          connectInstagram: function() {
             var deferred = $q.defer();
-            OAuth.popup('instagram', {cache:true}, function(error, result) { //cache means to execute the callback if the tokens are already present
+            OAuth.popup('instagram', {cache:true}, function(error, result) { 
                 if (!error) {
                     authResult = result;
                     console.log(result);
